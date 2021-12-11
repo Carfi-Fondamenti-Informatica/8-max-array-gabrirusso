32d4a2ac6ba37f48ca5ec26e20725161da5a1e42
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-12-11 16:40:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
-int main(){
-  return 0;
+using namespace std;
+#include "lib.h"
+
+int main() {
+    float b=0;
+    int n=0;
+    cin >> n;
+
+    float numeri [n];
+    for (int i=0; i < n; i++){
+        cin >> b;
+        numeri[i]=b;
+    }
+    cout<<maxarray(n, numeri)<<endl;
+    return 0;
 }
