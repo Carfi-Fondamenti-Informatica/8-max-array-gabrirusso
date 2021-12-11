d1d86cfb9f9b756fbb6bc0c8bb1486627090ae02
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-12-11 17:37:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+
 using namespace std;
+
 #include "lib.h"
 
+
 int main() {
+    float d=0;
+    int i=0;
     float b=0;
+    int c=0;
     int n=0;
+    int h=0;
     cin >> n;
 
     float numeri [n];
-    for (int i=0; i < n; i++){
+
+
+    while (i<n){
+        c=b;
         cin >> b;
         numeri[i]=b;
+        i++;
     }
-    cout<<maxarray(n, numeri[n])<<endl;
+
+
+    funzione(numeri, n);
+
+
+    cout << funzione(numeri, n) ;
     return 0;
 }
