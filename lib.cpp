b9b37b713313b02009715d74fd695fd461dc9d19
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-12-11 16:39:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+float maxarray(int n, float numeri[]){
+    float max=numeri[0];
+    for(int i=0;i<n;i++){
+
+        if(numeri[i+1]>max){
+            max=numeri[i+1];
+        }else{
+
+        }
+    }
+    return max;
+}
